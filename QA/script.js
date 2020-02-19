608aa2b4f67f87e2dea5a7efd73d7026711ac1b8
skwebs <51714932+skwebs@users.noreply.github.com>
2020-02-19 15:01:41 +0530

    Update script.js

--- QA/script.js
+++ QA/script.js
@@ -1,9 +1,10 @@
+var myObj = class9;
 var i,j,k,l,x="";
 
 for (i in myObj.chapter) {
 	x += "<h2 class=\"ch\">" + myObj.chapter[i].name + "</h2>";
 	for (j in myObj.chapter[i].section) {
-		x += "<h3 class=\"section\">"+myObj.chapter[i].section[j].name + "</h3>";
+		x += "<h4 class=\"section\">"+myObj.chapter[i].section[j].name + "</h4>";
 		x +="<table>";
 		for (k in myObj.chapter[i].section[j].quesAns) {
 			x += "<tr><td class=\"quesSec\">";
@@ -16,4 +17,8 @@ for (i in myObj.chapter) {
 		
 	}
 }
-document.getElementById("content").innerHTML = x;
+//document.getElementById("content").innerHTML = x;
+contentDiv.innerHTML = x;
+document.querySelector("#print").addEventListener("click",()=>{
+window.print();
+})
